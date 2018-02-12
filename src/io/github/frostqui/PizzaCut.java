082c2e9e4ac669ceab5ac5f561fefb789aca1bc1
Frostqui <diego.lopezgarcia@hotmail.com>
2018-02-12 18:21:46 +0100

    Algorithm changes 	- For now, the algorithm returns the best slices for the pizza, and discard other slices

--- src/io/github/frostqui/PizzaCut.java
+++ src/io/github/frostqui/PizzaCut.java
@@ -40,40 +40,47 @@ public class PizzaCut {
 
 	public void solution() {
 
-		
 		for (int i = 0; i < pizza.getRows(); i++) {
-			for (int j = 0; j <= pizza.getColumns(); j++) {
-				//System.out.println(j);
+			for (int j = 0; j < pizza.getColumns(); j++) {
+				//System.out.println(i + " " + j);
 				createSlice(i, j);
-				
+
 			}
 		}
 
 	}
 
 	public void createSlice(int row, int column) {
-		//System.out.println(column);
+		// System.out.println(column);
 		for (int i = row; i < pizza.getCells_per_slice(); i++) {
 			for (int j = column; j < pizza.getCells_per_slice(); j++) {
+
+				int x = row + i;
+				int y = column + j;
+				//System.out.println(row + " " + column + " x " +  x + " " + y);
+
+				if (checkValidSlice(row, column, x, y)) {
+					
 				
-				if (checkValidSlice(i, j, row + i, column + j)) {
-					
-					int x = row + i;
-					int y = column + j;
-					int sizex = x - row; 
-					int sizey = y - column;
-					
-					
-					checkCount(i, j, x, y);
+
+					// System.out.println(i + " " + x + " x " + j + " " + y);
+					checkCount(row, column, x, y);
 				}
+
 			}
+
 		}
 
+		//System.out.println(slicesInCell.size());
 		for (int i = 0; i < slicesInCell.size(); i++) {
 
 			Slice s = slicesInCell.get(i);
-			int count = (s.getEnd_row() - s.getStarting_row() + 1) * (s.getEnd_column() - s.getStarting_column() + 1);
+			int count = ((s.getEnd_row() - s.getStarting_row()) + 1)
+					* ((s.getEnd_column() - s.getStarting_column()) + 1);
+			System.out.println(count + "   " + count2);
+			
 			if (count > count2) {
+				//System.out.println("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
 				slices.add(s);
 				count2 = count;
 			}
@@ -81,12 +88,19 @@ public class PizzaCut {
 			// System.out.println(count + " & " + count2);
 
 		}
+		count2 = 0;
 		slicesInCell.removeAll(slicesInCell);
+		
+		
 	}
 
 	public boolean checkValidSlice(int startRow, int startColumn, int endRow, int endColumn) {
-		if ((endRow - startRow) * (endColumn - startColumn) <= pizza.getCells_per_slice()
-				&& endRow <= pizza.getRows()  - 1 && endColumn <= pizza.getColumns()) {
+		// System.out.println(((endRow - startRow) + 1 ) * ((endColumn - startColumn) +
+		// 1));
+		// System.out.println(pizza.getCells_per_slice());
+		if ((endRow - startRow + 1) * (endColumn - startColumn + 1) <= pizza.getCells_per_slice()
+				&& endRow < pizza.getRows() && endColumn < pizza.getColumns()) {
+
 			return true;
 		} else {
 			return false;
@@ -94,35 +108,81 @@ public class PizzaCut {
 	}
 
 	public void checkCount(int startRow, int startColumn, int endRow, int endColumn) {
-		
-		
-		//System.out.println(startRow - endRow);
-		for (int i = startRow; i < endRow; i++) {
-			for (int j = startColumn; j < endColumn; j++) {
-				//System.out.println(i + " " + j);
-				System.out.println(pizza.getCells()[i][j].charAt(0));
-				if (pizza.getCells()[i][j].charAt(0) == 'T') {
-
-					tomato_count++;
-
-				} else {
-
-					msroom_count++;
-
-				}
-			}
-		
-
-		//System.out.println("Tomato: " + tomato_count + " Msroom: " + msroom_count);
-
-		if (tomato_count > pizza.getIng_per_slice() && msroom_count > pizza.getIng_per_slice()) {
-			slicesInCell.add(new Slice(startRow, startColumn, endRow, endColumn));
-		}
 
 		tomato_count = 0;
 		msroom_count = 0;
 		
+		System.out.println("Row "+ startRow  + " to " +  endRow);
+		System.out.println("Column "+ startColumn  + " to " +  endColumn);
+		
+		if (startRow != endRow && startColumn != endColumn) {
+			
+			for (int i = startRow; i <= endRow; i++) {
+				for (int j = startColumn; j <= endColumn; j++) {
+					
+					if (pizza.getCells()[i][j].charAt(0) == 'T') {
+						tomato_count++;
+					} else {
+
+						msroom_count++;
+					}
+				}
+
+			}
 		}
+		if(startRow == endRow && startColumn == endColumn) {
+			
+			if (pizza.getCells()[startRow][startColumn].charAt(0) == 'T') {
+				tomato_count++;
+			} else {
+
+				msroom_count++;
+			}
+		}
+
+		if (startRow == endRow && startColumn != endColumn) {
+			
+			
+
+			for (int j = startColumn; j <= endColumn; j++) {
+			
+				if (pizza.getCells()[startRow][j].charAt(0) == 'T') {
+					//System.out.println(tomato_count);
+					tomato_count++;
+				} else {
+					msroom_count++;
+				}
+			}
+
+		}
+
+		if (startColumn == endColumn && startRow != endRow) {
+			for (int i = startRow; i <= endRow; i++) {
+				
+				if (pizza.getCells()[i][startColumn].charAt(0) == 'T') {
+					tomato_count++;
+				} else {
+					msroom_count++;
+				}
+			}
+
+		}
+		
+		
+
+		 System.out.println("Tomato: " + tomato_count + " Msroom: " + msroom_count);
+		 
+		 System.out.println("-------------------------");
+		
+		if (tomato_count >= pizza.getIng_per_slice() && msroom_count >= pizza.getIng_per_slice()) {
+			//System.out.println("eeeeeeeee");
+			slicesInCell.add(new Slice(startRow, startColumn, endRow, endColumn));
+			//slices.add(new Slice(startRow,startColumn, endRow, endColumn));
+			
+		}
+
+		tomato_count = 0;
+		msroom_count = 0;
 
 		// slices.add(new Slice(startRow,startColumn, endRow, endColumn));
 
